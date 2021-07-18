3f330eaac267ea663e677f26a7c94045d3da8ad2
Matthew Sievers <mattersievers@gmail.com>
2021-07-18 00:13:45 -0500

    changed playerInfo to object, not array of objects. localStorage saves correctly now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
+//Creater elements
 let pageContentEl = document.querySelector("#page-content");
-let buttonCollectEl = document.querySelector("#button-collection")
+let buttonCollectEl = document.querySelector("#button-collection");
+let formEl = document.createElement("form");
+//Set initial parameters
 let gameTime = 75;
 let questionCounter = 0;
 let playerScore = 0; 
 let adjustTime = false;
+let playerInfo = {
+   initials: null,
+   highScore: null,
+};
+
+let savedInfo = [];
+for (var i = 0; i < 10 ; i++){
+  savedInfo[i] = {
+    initials: "__",
+    highScore: 0
+  }
+};  
+
+
+// List of questions. answerStat = true gives the correlating correct solution.
 let questionList = [
   {
     question: "question 1 text ",
@@ -24,6 +42,7 @@ let questionList = [
   },
 ];
 
+//starts timer
 function startTime () {
   let gameTimer = setInterval(function() {
     //wrong answer and time left
@@ -119,22 +138,19 @@ let buttonHandler = function(event) {
 };
 
 
-//event listener for button clicks
-buttonCollectEl.addEventListener("click", buttonHandler);
-
 let endGame = function (){
   //Load final screen text
   document.querySelector("#main-text").textContent = "The Quiz Is Done!";
   document.querySelector("#begin-text").textContent = "Your final score is: " + playerScore + "!";
   
   //Build form
-  let formEl = document.createElement("form");
   let labelEl = document.createElement("label");
   labelEl.textContent = "Input initials:";
   let inputEl = document.createElement("input");
   inputEl.textContent = "Your initials";
   inputEl.setAttribute("type","text");
   inputEl.setAttribute("maxlength",2);
+  inputEl.setAttribute("name","initials");
   let buttonEl = document.createElement("button");
   buttonEl.setAttribute("type", "submit");
   buttonEl.className = "btn initial-btn";
@@ -145,12 +161,61 @@ let endGame = function (){
   formEl.appendChild(inputEl);
   formEl.appendChild(buttonEl);
 
-  pageContentEl.appendChild(formEl);
+  buttonCollectEl.appendChild(formEl);
   removeQuestion();
+  
 
-}
+  saveGame();
+};
 
+let highScoreScreen = function(){
+
+};
+
+let saveGame = function (event) {
+  event.preventDefault();
+  //Get info from storage if it exists
+  if (localStorage.getItem("savedInfo")){
+  savedInfo = localStorage.getItem("savedInfo");
+  savedInfo = JSON.parse(savedInfo);
+  }
+ 
+  //Check to see if player score beats lowest highScore out of the 10 scores
+  if (playerScore > savedInfo[9].highScore) {
+    alert("Congratulation! You made the High Score List!")
+    //Retrieve intials from input and highScore from player score
+    playerInfo.initials = document.querySelector("input[name='initials']").value;
+    // Reject blank submissions.
+    if (!playerInfo.initials){
+      alert("Please enter intials to continue.");
+      return false;
+    }
+    playerInfo.highScore = playerScore;  
+    //push player stats into saves stats
+    savedInfo.push(playerInfo);
+    //Sort by highScore
+    savedInfo.sort(function(a, b) {return b.highScore - a.highScore});
+    //Reject the 11th so it saves 10 entries
+    savedInfo.splice(10,1);
+    //Save the new stat list
+    console.log(savedInfo); 
+    console.log(JSON.stringify(savedInfo));
+    localStorage.setItem("savedInfo", JSON.stringify(savedInfo));  
+  }
+  else {
+    alert("Sorry. You did not make the High Score List. Study a bit and try again.")
+    localStorage.setItem("savedInfo", JSON.stringify(savedInfo)); 
+  }
+};
+
+//event listener for button clicks
+
+buttonCollectEl.addEventListener("click", buttonHandler);
+
+formEl.addEventListener("submit", saveGame);
 /* 
+- needs to alert right/wrong 
+- put in some real JS questions
 
 input initials and save to localStorage
 
